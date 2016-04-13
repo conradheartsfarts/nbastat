dd5b5d6624609cadf50c5c1bb7f6fdefac6211ae
ckl2150 <conrad.liu21@gmail.com>
2016-04-13 00:18:50 -0400

    added clarifying comment

--- parseStatLine.m
+++ parseStatLine.m
@@ -2,6 +2,7 @@ filename = 'players_c_curryst01_gamelog_2016__pgl_basic.csv';
 
 fid = fopen(filename, 'r');
 
+%specify the delimiter for regexp
 expression = ',';
 
 %files begin with an empty line, so script must skip first line
